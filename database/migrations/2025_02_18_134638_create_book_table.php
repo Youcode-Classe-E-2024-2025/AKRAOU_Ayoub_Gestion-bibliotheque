77c0c72f3109bb945693bb86b7014aa3f2b4ca90
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-02-21 21:54:24 +0100

    creer les tables a travers les migrations

--- database/migrations/2025_02_18_134638_create_book_table.php
+++ database/migrations/2025_02_18_134638_create_book_table.php
@@ -11,13 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->string('author');
+            $table->text('description');
+            $table->decimal('price', 10, 2);
+            $table->string('image');
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('books');
     }
 };
