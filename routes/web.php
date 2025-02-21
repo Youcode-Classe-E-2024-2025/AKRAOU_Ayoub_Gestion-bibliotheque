0d63717be1dfa6e7c0e3ceb90bcc17cc5235a072
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-02-21 21:57:33 +0100

    mettre en place les routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class, 'index'])->name('login');
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('register', [AuthController::class, 'register']);
+
+Route::get('books', [BookController::class, 'index'])->name('books.index');
+Route::get('books/create', [BookController::class, 'create'])->name('books.create');
+Route::post('books', [BookController::class, 'store'])->name('books.store');
+Route::get('books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
+Route::put('books/{book}', [BookController::class, 'update'])->name('books.update');
+Route::delete('books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+Route::get('my-books', [BookController::class, 'myBooks'])->name('books.my_books');
+Route::post('books/{book}/borrow', [BookController::class, 'borrow'])->name('books.borrow');
+Route::post('books/{book}/return', [BookController::class, 'return'])->name('books.return');
+
+Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+Route::delete('admin/users/{user}', [AdminController::class, 'destroy'])->name('admin.users.destroy');
